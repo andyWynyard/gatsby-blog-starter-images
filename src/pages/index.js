ab5397462011b42efe1bc920259c360bfd6645de
andyWynyard <andrew.wynyard@gmail.com>
2018-10-18 09:57:02 +0200

    refactor css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
-import { css } from "react-emotion";
 import CTA from "../components/cta";
 import AllUX from "../components/allUXers";
 import "./index.css";
