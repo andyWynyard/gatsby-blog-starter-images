96f49dbd91f7711fadcebe0226eed847fe3499a4
andyWynyard <andrew.wynyard@gmail.com>
2018-10-26 14:55:02 +0200

    testttyyyyy 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
-        displayName: false
+        displayName: true
       }
     },
     `gatsby-remark-copy-linked-files`,
