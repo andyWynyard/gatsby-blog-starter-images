4452a9b3a83caea76606c3cb4b86ac4ff1ef1bbb
andyWynyard <andrew.wynyard@gmail.com>
2018-11-07 14:07:53 +0100

    fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-const path = require(`path`);
+const path = require(`path`)
 
-const { get } = require("axios");
-const crypto = require("crypto");
+// const { get } = require("axios");
+const crypto = require('crypto')
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions;
+  const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
       name: `slug`,
-      value: slug
-    });
+      value: slug,
+    })
   }
-};
+}
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
+  const { createPage } = actions
   return new Promise((resolve, reject) => {
     graphql(`
       {
@@ -40,64 +40,64 @@ exports.createPages = ({ graphql, actions }) => {
           context: {
             // Data passed to context is available
             // in page queries as GraphQL variables.
-            slug: node.fields.slug
-          }
-        });
-      });
+            slug: node.fields.slug,
+          },
+        })
+      })
 
-      resolve();
-    });
-  });
-};
+      resolve()
+    })
+  })
+}
 
-exports.sourceNodes = async ({ actions }) => {
-  const { createNode } = actions;
+// exports.sourceNodes = async ({ actions }) => {
+//   const { createNode } = actions;
 
-  // fetch raw data from the randomuser api
-  const fetchRandomUser = () => get(`https://randomuser.me/api/?results=500`);
-  // await for results
-  const res = await fetchRandomUser();
+//   // fetch raw data from the randomuser api
+//   const fetchRandomUser = () => get(`https://randomuser.me/api/?results=500`);
+//   // await for results
+//   const res = await fetchRandomUser();
 
-  // map into these results and create nodes
-  res.data.results.map((user, i) => {
-    // Create your node object
-    const userNode = {
-      // Required fields
-      id: `${i}`,
-      parent: `__SOURCE__`,
-      internal: {
-        type: `RandomUser` // name of the graphQL query --> allRandomUser {}
-        // contentDigest will be added just after
-        // but it is required
-      },
-      children: [],
+//   // map into these results and create nodes
+//   res.data.results.map((user, i) => {
+//     // Create your node object
+//     const userNode = {
+//       // Required fields
+//       id: `${i}`,
+//       parent: `__SOURCE__`,
+//       internal: {
+//         type: `RandomUser` // name of the graphQL query --> allRandomUser {}
+//         // contentDigest will be added just after
+//         // but it is required
+//       },
+//       children: [],
 
-      // Other fields that you want to query with graphQl
-      gender: user.gender,
-      name: {
-        title: user.name.title,
-        first: user.name.first,
-        last: user.name.last
-      },
-      picture: {
-        large: user.picture.large,
-        medium: user.picture.medium,
-        thumbnail: user.picture.thumbnail
-      }
-      // etc...
-    };
+//       // Other fields that you want to query with graphQl
+//       gender: user.gender,
+//       name: {
+//         title: user.name.title,
+//         first: user.name.first,
+//         last: user.name.last
+//       },
+//       picture: {
+//         large: user.picture.large,
+//         medium: user.picture.medium,
+//         thumbnail: user.picture.thumbnail
+//       }
+//       // etc...
+//     };
 
-    // Get content digest of node. (Required field)
-    const contentDigest = crypto
-      .createHash(`md5`)
-      .update(JSON.stringify(userNode))
-      .digest(`hex`);
-    // add it to userNode
-    userNode.internal.contentDigest = contentDigest;
+//     // Get content digest of node. (Required field)
+//     const contentDigest = crypto
+//       .createHash(`md5`)
+//       .update(JSON.stringify(userNode))
+//       .digest(`hex`);
+//     // add it to userNode
+//     userNode.internal.contentDigest = contentDigest;
 
-    // Create node with the gatsby createNode() API
-    createNode(userNode);
-  });
+//     // Create node with the gatsby createNode() API
+//     createNode(userNode);
+//   });
 
-  return;
-};
+//   return;
+// };
